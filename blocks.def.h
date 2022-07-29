a74da30e4a1b23655b3a382ac30e55a5f3fc2e72
ChowaniakBartosz <chowaniak.bartosz@protonmail.com>
2022-07-29 15:58:03 +0200

    changed sb-volume signal and order of modules

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-	{"", "sb-date",		20,	0},
-	{"", "sb-volume",	0,	0},
+	{"", "sb-volume",	0,	10},
 	{"", "sb-battery",	60,	0},
+	{"", "sb-date",		20,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
