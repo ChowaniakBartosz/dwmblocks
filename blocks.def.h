ef48f75001ac3cae4bb31e1234315c917c06fafc
Bartosz Chowaniak <chowaniak.bartosz@gmail.com>
2024-01-17 23:58:27 +0100

    change commands to local scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%b %d (%a) %I:%M%p'",					60,		0},
+	{"BAT: ", "sb-battery",							60,		0},
+	{"", "sb-datetime",					60,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
